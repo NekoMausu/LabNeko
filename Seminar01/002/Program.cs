e16fd0bae286aee9879afb96d5f1c86969d1565e
neko <dima-bikmaev@mail.ru>
2022-09-10 16:33:17 +0400

    002

--- Seminar01/002/Program.cs
+++ Seminar01/002/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа
+// и выдает, какое число большее, а какое меньшее
+Console.WriteLine("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+if (a < b) 
+{
+    Console.WriteLine($"max {b}");
+    Console.WriteLine($"min {a}");
+}
+else 
+{
+    Console.WriteLine($"max {a}");
+    Console.WriteLine($"min {b}");
+}
