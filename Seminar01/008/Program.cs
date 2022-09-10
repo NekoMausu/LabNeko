ff930aca37bf9f2492874df27836ff65a0f59cf8
neko <dima-bikmaev@mail.ru>
2022-09-10 17:16:12 +0400

    008

---
+++ Seminar01/008/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N)
+// а на выходе показывает все четные числа от 1 до N
+Console.WriteLine("Введите число ");
+int N = int.Parse(Console.ReadLine());
+int S = 2;
+while (S<N) 
+{
+Console.Write($"{S}, ");
+S=S+2;
+}
