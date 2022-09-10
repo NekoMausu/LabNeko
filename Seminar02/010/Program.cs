7b8c54433847b6315e0981adaee0af4506f53226
neko <dima-bikmaev@mail.ru>
2022-09-10 23:55:15 +0400

    s3

--- Seminar02/010/Program.cs
+++ Seminar02/010/Program.cs
@@ -2,6 +2,6 @@
 // и на выходе показывает вторую цифру этого числа
 Console.WriteLine("Введите трехзначное число");
 int num = int.Parse(Console.ReadLine());
-int num1 = num % 10;
-int num2 = num % 100;
-Console.WriteLine((num2-num1)/10);
+int num1 = num / 10;
+num = num1 % 10;
+Console.WriteLine(num);
