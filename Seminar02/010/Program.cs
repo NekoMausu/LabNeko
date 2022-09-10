272da0ea5021d80361b1b7bec3d1c82bea71773b
neko <dima-bikmaev@mail.ru>
2022-09-10 22:22:05 +0400

    010

---
+++ Seminar02/010/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число
+// и на выходе показывает вторую цифру этого числа
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+int num1 = num % 10;
+int num2 = num % 100;
+Console.WriteLine((num2-num1)/10);
