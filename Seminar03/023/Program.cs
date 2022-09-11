f549b01599f891fd694ab79e21c4af1066634a0d
neko <dima-bikmaev@mail.ru>
2022-09-11 20:33:31 +0400

    023

---
+++ Seminar03/023/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдает таблицу кубов чисел от 1 до N
+Console.WriteLine("Введите число ");
+int N = int.Parse(Console.ReadLine());
+int ind = 1;
+while (ind <= N)
+{
+    Console.Write($"{Math.Pow(ind, 3)}, ");
+    ind++;
+}
