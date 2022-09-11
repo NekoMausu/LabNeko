bd8cc17ab268bc924c3706b59cb219431187c274
neko <dima-bikmaev@mail.ru>
2022-09-11 20:16:27 +0400

    019

---
+++ Seminar03/019/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом. 12321
+Console.WriteLine("Введите пятизначное число ");
+int num = int.Parse(Console.ReadLine());
+if (num % 10 == num / 10000 && (num % 100) / 10 == (num / 1000) % 10) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+
