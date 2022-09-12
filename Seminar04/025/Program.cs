6a7b44d38ee6ed737dba6e214a71c28697616264
neko <dima-bikmaev@mail.ru>
2022-09-12 17:00:34 +0400

    fix 027

--- Seminar04/025/Program.cs
+++ Seminar04/025/Program.cs
@@ -1,25 +1,18 @@
 ﻿// Напишите цикл, который принимает на вход два числа (А и В)
 // и возводит число А в натуральную степень В
-void POW(int[] an)
+int POW(int numberA, int numberB)
 {
-    int num = an[0];
-    for (int i = 1; i < an[1]; i++)
+    int result = numberA;
+    for (int i = 1; i < numberB; i++)
     {
-        an[0] = num * an[0];
+        result*=numberA;
     }
-    Console.WriteLine(an[0]);
-
+    return result;
 }
-
-
 
 Console.WriteLine("Введите число А ");
 int A = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число B ");
 int B = int.Parse(Console.ReadLine());
-if (B < 0) B=-B;
-int[] arrav = new int[2];
-arrav[0] = A;
-arrav[1] = B;
-POW(arrav);
-
+if (B < 0) B = -B;
+Console.WriteLine(POW(A,B));
