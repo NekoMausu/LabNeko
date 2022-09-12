8350422ed16933c18129457329d6f9234dd9fd6b
neko <dima-bikmaev@mail.ru>
2022-09-12 14:25:18 +0400

    027

---
+++ Seminar04/027/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите функцию и запустите ее, которая принимает на вход число
+// и выдает сумму цифр в числе
+int SUM(int B)
+{
+    int res = 0;
+    while (B != 0)
+    {
+        res = B % 10 + res;
+        B = B / 10;
+    }
+    return res;
+}
+
+
+
+
+Console.WriteLine("Введите число ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine($"{SUM(A)}");
