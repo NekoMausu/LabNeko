decc3082b4d13bb16df9885c64beebe35cb7d589
neko <dima-bikmaev@mail.ru>
2022-09-12 17:44:44 +0400

    sa

--- Seminar04/029/Program.cs
+++ Seminar04/029/Program.cs
@@ -3,16 +3,20 @@
 int[] FillArray(int numA, int numB, int numN)
 {
     int[] massiv = new int[numN];
-    for (int i = 0; i < NumN; i++)
+    for (int i = 0; i < numN; i++)
     {
         massiv[i] = new Random().Next(numA, numB);
-        Console.Write($"{massiv[i]}, ");
     }
+    return massiv;
 }
 
-void WriteArray(massiv[])
+
+void WriteArray(int[] massiv)
 {
-    
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        Console.Write($"{massiv[i]}, ");
+    }
 }
 
 
@@ -23,4 +27,4 @@ Console.WriteLine("Введите мнимальное число ");
 int A = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите максимальное число -1 ");
 int B = int.Parse(Console.ReadLine());
-POW(arrav);
+WriteArray(FillArray(A,B,N));
