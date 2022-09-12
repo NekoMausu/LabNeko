e739c3b219fb6382a529b501eda7918c41b7f510
neko <dima-bikmaev@mail.ru>
2022-09-12 16:20:31 +0400

    STAR

---
+++ Seminar04/STAR/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите функцию, которая принимает одно число - высоту елочки
+// и рисует ее в консоли звездочками.
+void tree(int Depth)
+{
+    int d = 1;
+    int v = 1;
+    int z=Depth;
+    for (int i = 1; i <= Depth; i++)
+    {
+        d = v;
+        while (d != 0)
+        {
+            while (z>1)
+            {
+                z--;
+                Console.Write(" ");
+            }
+            Console.Write("*");
+            d--;
+            
+
+        }
+        v += 2;
+        Console.WriteLine(" ");
+        z=Depth-i;
+    }
+}
+Console.WriteLine("Введите высоту елочки ");
+int H = int.Parse(Console.ReadLine());
+tree(H);
