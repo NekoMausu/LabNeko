c388c514594fc05041e648b2f5dc2aa945766663
NekoMausu <dima-bikmaev@mail.ru>
2022-09-18 20:24:13 +0300

    036

---
+++ Seminar05/036/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечетных позициях.
+
+void Sum(int[] massiv)
+{
+    int sum = 0;
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        sum += i % 2 == 0 ? 0 : massiv[i];
+    }
+    Console.WriteLine($"{sum}");
+}
+
+
+int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 100);
+
+Console.WriteLine($"{string.Join(" ", array)}");
+Sum(array);
