61c140481cc324e5a2edb6d0454aa9eafa2f647c
NekoMausu <dima-bikmaev@mail.ru>
2022-09-25 14:16:33 +0300

    068

---
+++ Seminar09/068/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу вычисления функции Аккермана
+// Даны два неотрицательных числа m и n
+
+int PW(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return PW(m - 1, 1);
+    if (m > 0 && n > 0) return PW(m - 1, PW(m, n - 1));
+    return PW(n, m);
+}
+
+int res = PW(3, 5);
+Console.WriteLine($"{res}");
