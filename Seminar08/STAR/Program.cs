b2679dc150b743044d325249e85d254bfcdae7d5
NekoMausu <dima-bikmaev@mail.ru>
2022-09-25 15:08:09 +0300

    star

--- Seminar08/STAR/Program.cs
+++ Seminar08/STAR/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿// функция sqrt
+
+int SQRT(int N)
+{
+    int start = 1;
+    int corentN = N;
+    while (start + 1 != N)
+    {
+        if ((((N + start) / 2) * ((N + start) / 2)) > corentN)
+        {
+            N = ((N + start) / 2);
+        }
+        else if ((((N + start) / 2) * ((N + start) / 2)) < corentN)
+        {
+            start = ((N + start) / 2);
+        }
+        else return (N+start)/2;
+
+    }
+    return start;
+}
+
+int a = int.Parse(Console.ReadLine());
+int res = SQRT(a);
+Console.Write($"результат {res}");
